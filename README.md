ff6580ce94fba17d054597c5af583cc66588b4bf
devkaan <abdikaangrbz@gmail.com>
2021-05-09 14:27:05 +0300

    performans iyilesirmeleri

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-## version 0.0.2
+## güncel versiyon 0.0.3
 # Gerekli modüller: discord.js dotenv
-## Kurulum için _*npm run setup*_
-### Çalıştırmak için node _app.js_ (botu sunucunuza ekleyip, botun tokenini .env dosyasına yazdıktan sonra)
-### Kullanılabilen komutlar !tas !kagit !makas !sifirla 
+## Kurulum için  ```npm run setup```
+### Çalıştırmak için ```node app.js``` (botu sunucunuza ekleyip, botun tokenini .env dosyasına yazdıktan sonra)
+### Kullanılabilen komutlar ```!tas``` ```!kagit``` ```!makas``` ```!sifirla``` 
+
+
+# Yenilikler: 
+
+### version 0.0.3
+* [+] artık her sunucu için farklı skorlar.
+
+### version 0.0.2
+* [+] bot doğru çalışmıyordu. düzeltildi.
+
+### version 0.0.1
+* [+] bot çalışır halde.
+
--- app.js
+++ app.js
@@ -6,78 +6,96 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-console.log('                                                        ');
-console.log('--------------------------------------------------------');
-console.log("Taş kağıt makas botu github@devkaan tarafından. versiyon 0.0.2");
+console.log('\n--------------------------------------------------------');
+console.log("Taş kağıt makas botu github@devkaan tarafından. versiyon 0.0.3");
 console.log("Başlamak için !tas !kagit veya !makas. Skoru sıfırlamak için !sifirla");
-console.log('--------------------------------------------------------');
-console.log('                                                        ');
+console.log('--------------------------------------------------------\n');
 
+var pool = [];
+var users = [];
 var pcScore = 0;
 var humanScore = 0;
 // const command = "!bot";
 arr = ['taş', 'kağıt', 'makas'];
+var serverid
+var servername
+var userid
+serverchanged = false
+const botid = 840532235924013086;
 client.on('message', msg => {
-    if (msg.content === "!sifirla") {
-        pcScore = 0, humanScore = 0
-        console.log('--------------------------------------------------------');
-        console.log('Skorlar sıfırlandı. Başlamak için !tas !kagit veya !makas');
-        console.log('--------------------------------------------------------');
-        msg.channel.send('Skorlar sıfırlandı. Başlamak için !tas !kagit veya !makas');
-    }
-    else {
-        if (msg.content === ("!tas")) {
-            randomInt = Math.floor(Math.random() * 3);
-            pcDecision = arr[randomInt];
-            console.log(pcDecision);
-            if (arr[0] == pcDecision) {
-                msg.reply("Berabere!");
+
+    userid = msg.author.id;
+
+
+    if (userid != botid) {
+       serverid = msg.guild.id
+        if (msg.content === "!sifirla") {
+            var servername = msg.guild.name
+            delete pool[serverid]
+
+            console.log('\n--------------------------------------------------------');
+            console.log("ID'si  " + serverid + "  olan sunucu (" + servername + ")'nun skoru sıfırlandı. Başlamak için !tas !kagit veya !makas");
+            console.log('--------------------------------------------------------\n');
+            msg.channel.send('Skorlar sıfırlandı. Başlamak için !tas !kagit veya !makas');
+        }
+        else {
+            if (msg.content === ("!tas")) {
+                randomInt = Math.floor(Math.random() * 3);
+                pcDecision = arr[randomInt];
+                // console.log(pcDecision);
+                if (arr[0] == pcDecision) {
+                    msg.reply("Berabere!");
+                }
+                else if (arr[1] == pcDecision) {
+                    pcScore++;
+                    msg.reply("Bilgisayar (" + pcDecision + ") kazandı. \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+                }
+                else if (arr[2] == pcDecision) {
+                    humanScore++;
+                    msg.reply("Sen kazandın. Bilgisayar (" + pcDecision + ") \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+                }
+
             }
-            else if (arr[1] == pcDecision) {
-                pcScore++;
-                msg.reply("Bilgisayar (" + pcDecision + ") kazandı. \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+            else if (msg.content === ("!kagit")) {
+                randomInt = Math.floor(Math.random() * 3);
+                pcDecision = arr[randomInt];
+                // console.log(pcDecision);
+                if (arr[1] == pcDecision) {
+                    msg.reply("Berabere!");
+                }
+                else if (arr[2] == pcDecision) {
+                    pcScore++;
+                    msg.reply("Bilgisayar (" + pcDecision + ") kazandı. \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+                }
+                else if (arr[0] == pcDecision) {
+                    humanScore++;
+                    msg.reply("Sen kazandın. Bilgisayar (" + pcDecision + ") \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+                }
+
             }
-            else if (arr[2] == pcDecision) {
-                humanScore++;
-                msg.reply("Sen kazandın. Bilgisayar (" + pcDecision + ") \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+            else if (msg.content === ("!makas")) {
+                randomInt = Math.floor(Math.random() * 3);
+                pcDecision = arr[randomInt];
+                // console.log(pcDecision);
+                if (arr[2] == pcDecision) {
+                    msg.reply("Berabere!");
+                }
+                else if (arr[0] == pcDecision) {
+                    pcScore++;
+                    msg.reply("Bilgisayar (" + pcDecision + ") kazandı. \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+                }
+                else if (arr[1] == pcDecision) {
+                    humanScore++;
+                    msg.reply("Sen kazandın. Bilgisayar (" + pcDecision + ") \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
+                }
             }
 
+            pool[serverid] = {
+                pcScore,
+                humanScore
+            }
         }
-        else if (msg.content === ("!kagit")) {
-            randomInt = Math.floor(Math.random() * 3);
-            pcDecision = arr[randomInt];
-            console.log(pcDecision);
-            if (arr[1] == pcDecision) {
-                msg.reply("Berabere!");
-            }
-            else if (arr[2] == pcDecision) {
-                pcScore++;
-                msg.reply("Bilgisayar (" + pcDecision + ") kazandı. \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
-            }
-            else if (arr[0] == pcDecision) {
-                humanScore++;
-                msg.reply("Sen kazandın. Bilgisayar (" + pcDecision + ") \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
-            }
-
-        }
-        else if (msg.content === ("!makas")) {
-            randomInt = Math.floor(Math.random() * 3);
-            pcDecision = arr[randomInt];
-            console.log(pcDecision);
-            if (arr[2] == pcDecision) {
-                msg.reply("Berabere!");
-            }
-            else if (arr[0] == pcDecision) {
-                pcScore++;
-                msg.reply("Bilgisayar (" + pcDecision + ") kazandı. \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
-            }
-            else if (arr[1] == pcDecision) {
-                humanScore++;
-                msg.reply("Sen kazandın. Bilgisayar (" + pcDecision + ") \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
-            }
-
-        }
+        console.log('\npool =>', pool);
     }
 });
 

6c4533226384e9c33a68304d92c23773c9c60265
devkaan <abdikaangrbz@gmail.com>
2021-05-18 14:25:14 +0300

    new command

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-## güncel versiyon 0.0.3
+## güncel versiyon 0.0.4
 # Gerekli modüller: discord.js dotenv
 ## Kurulum için  ```npm run setup```
 ### Çalıştırmak için ```node app.js``` (botu sunucunuza ekleyip, botun tokenini .env dosyasına yazdıktan sonra)
-### Kullanılabilen komutlar ```!tas``` ```!kagit``` ```!makas``` ```!sifirla``` 
+### Kullanılabilen komutlar ```!tas``` ```!kagit``` ```!makas``` ```!sifirla``` ```!skor``` 
 
 
 # Yenilikler: 
+
+### version 0.0.4
+* [+] !skor komutuyla skorunuzu öğrenebilirsiniz.
 
 ### version 0.0.3
 * [+] artık her sunucu için farklı skorlar.
--- app.js
+++ app.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 console.log('\n--------------------------------------------------------');
-console.log("Taş kağıt makas botu github@devkaan tarafından. versiyon 0.0.3");
-console.log("Başlamak için !tas !kagit veya !makas. Skoru sıfırlamak için !sifirla");
+console.log("Taş kağıt makas botu github@devkaan tarafından. versiyon 0.0.4");
+console.log("Başlamak için !tas !kagit !makas veya !skor. Skoru sıfırlamak için !sifirla");
 console.log('--------------------------------------------------------\n');
 
 var pool = [];
@@ -22,36 +22,87 @@ var servername
 var userid
 serverchanged = false
 const botid = 840532235924013086;
+var isfirst = true;
+var pcScore, humanScore
 client.on('message', msg => {
 
     userid = msg.author.id;
 
 
     if (userid != botid) {
-       serverid = msg.guild.id
+        serverid = msg.guild.id
         if (msg.content === "!sifirla") {
             var servername = msg.guild.name
+            isfirst = true
             delete pool[serverid]
 
             console.log('\n--------------------------------------------------------');
-            console.log("ID'si  " + serverid + "  olan sunucu (" + servername + ")'nun skoru sıfırlandı. Başlamak için !tas !kagit veya !makas");
+            console.log("ID'si  " + serverid + "  olan sunucu (" + servername + ")'nun skoru sıfırlandı. Başlamak için !tas !kagit !makas veya !skor");
             console.log('--------------------------------------------------------\n');
-            msg.channel.send('Skorlar sıfırlandı. Başlamak için !tas !kagit veya !makas');
+            msg.channel.send('Skorlar sıfırlandı. Başlamak için !tas !kagit !makas veya !skor');
+        }
+        else if (msg.content === "!skor") {
+            var servername = msg.guild.name
+            var a, b
+            try {
+                a = pool[serverid].pcScore
+            } catch (err) {
+                a = 0;
+            }
+            try {
+                b = pool[serverid].humanScore
+            } catch (error) {
+                b = 0
+            }
+            console.log('\n--------------------------------------------------------');
+            console.log("ID'si  " + serverid + "  olan sunucu (" + servername + ")'nun skoru:\nSen: " + a + " Bilgisayar: " + b);
+            console.log('--------------------------------------------------------\n');
+            msg.channel.send("SKOR:\nSen: " + a + " Bilgisayar: " + b);
         }
         else {
+            if (isfirst) {
+                isfirst = false;
+                console.log('isfirst = false');
+                pool[serverid] = {
+                    pcScore: 0,
+                    humanScore: 0
+                }
+            }
             if (msg.content === ("!tas")) {
                 randomInt = Math.floor(Math.random() * 3);
                 pcDecision = arr[randomInt];
+
+
                 // console.log(pcDecision);
                 if (arr[0] == pcDecision) {
                     msg.reply("Berabere!");
                 }
                 else if (arr[1] == pcDecision) {
-                    pcScore++;
+                    try {
+                        pcScore = pool[serverid].pcScore
+                    } catch (error) {
+                        pcScore = 0
+                    }
+                    try {
+                        pcScore++
+                        pool[serverid].pcScore = (pcScore)
+                    } catch (error) {
+                        pool[serverid].pcScore = 1
+                    }
                     msg.reply("Bilgisayar (" + pcDecision + ") kazandı. \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
                 }
                 else if (arr[2] == pcDecision) {
-                    humanScore++;
+                    try {
+                        humanScore = pool[serverid].humanScore
+                    } catch (error) {
+                        humanScore = 0
+                    }
+                    try {
+                        humanScore++;
+                        pool[serverid].humanScore = (humanScore)
+                    } catch (error) {
+                        pool[serverid].humanScore = 1
+                    }
                     msg.reply("Sen kazandın. Bilgisayar (" + pcDecision + ") \nSKOR sen: " + humanScore + " bilgisayar: " + pcScore);
                 }
 
@@ -90,10 +141,7 @@ client.on('message', msg => {
                 }
             }
 
-            pool[serverid] = {
-                pcScore,
-                humanScore
-            }
+            
         }
         console.log('\npool =>', pool);
     }

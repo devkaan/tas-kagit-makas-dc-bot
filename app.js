b70dd962519746d5cea18f4456e2ee5537ad7f0d
devkaan <abdikaangrbz@gmail.com>
2021-06-08 16:10:00 +0300

    a

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ console.log('--------------------------------------------------------\n');
 
 
 client.on('ready', async () => {
+    console.log(`LOGGGGGGGGGGGGGG`);
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity("-yardim", {
         type: "LISTENING",
